68df840932aaba39947f6e8b1c362055a36db296
HLOP159 <kovalev159357@gmail.com>
2022-09-09 11:45:22 +0300

    HW 1-15-3 with particle system on finish
    HW 1-15-3 with particle system on finish

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -2,9 +2,16 @@ using UnityEngine;
 
 public class Finish : MonoBehaviour
 {
+    private void Start()
+    {
+        ParticleSystem particleSystem = gameObject.GetComponent<ParticleSystem>(); // выключает систему частиц финиша
+        particleSystem.Stop();
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if (!collision.collider.TryGetComponent(out Player player)) return;
         player.ReachFinish();
+        ParticleSystem particleSystem = gameObject.GetComponent<ParticleSystem>(); // включает систему частиц финиша
+        particleSystem.Play();
     }
 }
